1b458ac4ce6548b67f54864c9d4451a069e5a372
Taylorshmailor <taylorshmailor@gmail.com>
2025-01-29 20:51:20 -0500

    Moved everything into frontend folder

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./home.css";
+
+const Home = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default Home;
